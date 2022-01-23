c1468536a360f34ba2a2c53f64044ae7fb3311f3
Felipeek <felipeek@hotmail.com>
2022-01-22 22:52:21 -0300

    win: add fp:fast flag to build

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set COMPILER_FLAGS=/MT /nologo /D_CRT_SECURE_NO_WARNINGS /I../include /I../include/freetype /Zi /Feraw-physics.exe /O2 /wd4576 /EHsc /std:c++latest
+set COMPILER_FLAGS=/MT /nologo /D_CRT_SECURE_NO_WARNINGS /I../include /I../include/freetype /Zi /Feraw-physics.exe /O2 /wd4576 /EHsc /std:c++latest /fp:fast
 set LIBRARIES=opengl32.lib ws2_32.lib user32.lib ole32.lib Shell32.lib gdi32.lib winmm.lib kernel32.lib ../lib/win64/glew32.lib ../lib/win64/glfw3dll.lib
 set FILES=../src/*.cpp ../src/examples/*.cpp ../src/physics/*.cpp ../src/render/*.cpp ../src/vendor/*.cpp
 
